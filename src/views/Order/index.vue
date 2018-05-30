e3348220cf86057456ecebc6d1a6f58553848010
欧元 <beyondouyuan@users.noreply.github.com>
2018-05-30 18:16:16 +0800

    增加头部tab

--- src/views/Order/index.vue
+++ src/views/Order/index.vue
@@ -1,27 +1,67 @@
 <template>
     <div class="order-container">
         <Loading v-show="loading" />
-            <vue-better-scroll
-                    style="height:300px" 
-                    class="wrapper"
-                    ref="scroll"
-                    :scrollbar="scrollbarObj"
-                    :pullUpLoad="pullUpLoadObj"
-                    :startY="parseInt(startY)"
-                    @pullingUp="pullingUp"
-                    v-if="orderList.length !=0">
-                    <div v-for="(item, index) of orderList" :key="index" class="order-list">
-                      <div class="title-box">
-                          <h2>{{item.merchantName}}</h2>
-                      </div>
-                      <div class="detail-box">
-                          <div class="detail-item"><span>付款金额</span><span class="amount-value">{{item.payAmount}}</span></div>
-                          <div class="detail-item"><span>订单编号</span><span>{{item.gatheringOrderNo}}</span></div>
-                          <div class="detail-item"><span>付款时间</span><span>{{item.payTime | parseTime}}</span></div>
-                      </div>
-                  </div>
-              </vue-better-scroll>
+            <header class="tab-box">
+              <nav class="navigation">
+                <router-link
+                  to="/order"
+                 :class="{active: $route.query.tab == 'cons' || !$route.query.tab}">消费订单</router-link>
+                <router-link
+                  to="/order?tab=recharge"
+                  :class="{active: $route.query.tab == 'recharge'}">充值订单</router-link>
+                <router-link
+                  to="/order?tab=shell"
+                  :class="{active: $route.query.tab == 'shell'}">转为享贝</router-link>
+              </nav>
+            </header>
+              <div class="order-wrapper" v-if="orderList.length !=0">
+                  <vue-better-scroll
+                      style="height:300px" 
+                      class="wrapper"
+                      ref="scroll"
+                      :scrollbar="scrollbarObj"
+                      :pullUpLoad="pullUpLoadObj"
+                      :startY="parseInt(startY)"
+                      @pullingUp="pullingUp"
+                      >
+                      <div v-for="(item, index) of orderList" :key="index" class="order-list">
+                        <div class="order-content" v-if="$route.query.tab == 'cons' || !$route.query.tab">
+                          <div class="title-box">
+                            <h2>{{item.merchantName}}</h2>
+                          </div>
+                          <div class="detail-box">
+                              <div class="detail-item"><span>付款金额</span><span class="amount-value">&yen;{{item.payAmount}}</span></div>
+                              <div class="detail-item"><span>订单编号</span><span>{{item.gatheringOrderNo}}</span></div>
+                              <div class="detail-item"><span>付款时间</span><span>{{item.payTime | parseTime}}</span></div>
+                          </div>
+                        </div>
+                        <div class="order-content" v-else-if="$route.query.tab == 'recharge'">
+                          <div class="title-box">
+                            <h2>{{item.merchantName}}</h2>
+                          </div>
+                          <div class="detail-box">
+                              <div class="detail-item"><span>充值金额</span><span class="amount-value">&yen;{{item.payAmount}}</span></div>
+                              <div class="detail-item"><span>获得金额</span><span class="amount-value">&yen;{{item.payAmount}}</span></div>
+                              <div class="detail-item"><span>订单编号</span><span>{{item.gatheringOrderNo}}</span></div>
+                              <div class="detail-item"><span>付款时间</span><span>{{item.payTime | parseTime}}</span></div>
+                          </div>
+                        </div>
+                        <div class="order-content" v-else>
+                          <div class="title-box">
+                            <h2>{{item.merchantName}}</h2>
+                          </div>
+                          <div class="detail-box">
+                              <div class="detail-item"><span>转入金额</span><span class="amount-value">&yen;{{item.payAmount}}</span></div>
+                              <div class="detail-item"><span>相当享贝</span><span class="amount-value">&yen;{{item.payAmount}}</span></div>
+                              <div class="detail-item"><span>订单编号</span><span>{{item.gatheringOrderNo}}</span></div>
+                              <div class="detail-item"><span>转入时间</span><span>{{item.payTime | parseTime}}</span></div>
+                          </div>
+                        </div>
+                    </div>
+                </vue-better-scroll>
+              </div>
               <div v-else class="no-data">
+                <img src="../../assets/images/icon_nodata@2x.png" alt="暂无数据">
                 <p>暂无数据</p>
               </div>
     </div>
@@ -31,6 +71,17 @@
     import { requestFindMyOrder } from '@/api'
     import { Toast } from 'mint-ui'
     const requestCode = '0000'
+    const orderList = [{
+      merchantName: 'mmmm',
+      payAmount: 12,
+      gatheringOrderNo: '213456789',
+      payTime: '2018-01-01'
+    },{
+      merchantName: 'llllll',
+      payAmount: 122,
+      gatheringOrderNo: '098765432',
+      payTime: '2018-09-01'
+    }]
     export default {
         name: 'Order',
         components: {
@@ -38,14 +89,16 @@
         },
         data() {
             return {
-                loading: true,
+                // orderList,
+                loading: false,
                 orderList: [],
                 nextPage: 1,
                 totalPage: 1,
                 pageNo: 1,
                 listQuery: {
                   pageNum: 1,
-                  pageSize: 5
+                  pageSize: 5,
+                  recordType: 0
                 },
                 allLoaded: false,
                 scrollbarObj: {
@@ -67,7 +120,29 @@
         created() {
             this.fetchFindMyOrder()
         },
+        watch: {
+          // 路由监控
+          $route(to, from) {
+            const recordType  = this.transTab(this.$route.query.tab)
+            // 更新查询条件
+            this.listQuery.pageNum = 1
+            this.listQuery.recordType = recordType
+            this.fetchFindMyOrder()
+          }
+        },
         methods: {
+          transTab(tab) {
+            switch(tab) {
+              case 'cons':
+                return 0
+              case 'recharge':
+                return 1
+              case 'shell':
+                return 2
+              default:
+                return 0
+            }
+          },
           pullingUp() {
                 if (this.nextPage > this.totalPage) {
                     this.$refs.scroll.forceUpdate(false);
@@ -81,6 +156,7 @@
              const condition = {
               ...this.listQuery
             }
+            this.loading = true
             requestFindMyOrder(condition).then(res => {
               const endTime = Date.now() // 请求结束
               if (res.code == requestCode) {
@@ -115,29 +191,65 @@
         width: 100%;
         min-height: 100vh;
         height: 100%;
-        // padding-bottom: 186px;
         background: #F5F5F5;
         position: relative;
         .no-data {
           width: 100%;
-          height: 100vh;
+          height: calc(100vh - 100px); // 去除tab高度
           display: flex;
-          align-items: center;
-          display: flex;
+          flex-direction: column;
           align-items: center;
           justify-content: center;
           p {
-            font-size: 36px;
-            color:#CCCCCC;
+            margin-top: 20px;
+            font-size: 28px;
+            color:#999999;
           }
         }
+        .navigation {
+            width: 100%;
+            height: 100px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            background:rgba(255,255,255,1);
+            a {
+              flex: 1;
+              display: flex;
+              height: 100%;
+              align-items: center;
+              justify-content: center;
+              font-size:28px;
+              font-family:PingFangSC-Regular;
+              color:rgba(153,153,153,1);
+              position: relative;
+              &.active {
+                  color:rgba(51,51,51,1);
+                  &::after {
+                    content: '';
+                    position: absolute;
+                    top: 100%;
+                    left: 50%;
+                    transform: translateX(-50%);
+                    width:80px;
+                    height:4px;
+                    background:rgba(51,51,51,1);
+                  }
+              }
+            }
+          }
+          .order-wrapper {
+            margin-top: 20px;
+          }
         .order-list {
-            width:686px; 
-            margin: 0 auto 32px auto;
-            padding: 39px 0 40px 30px;
+            margin: 0 auto 20px auto;
+            padding: 0 30px 0 32px;
             background:rgba(255,255,255,1);
             &:not(:first-child) {
-                margin: 32px auto;
+                // margin: 32px auto;
+            }
+            .order-content {
+              padding: 30px 0 30px 0;
             }
         }
         .title-box {
